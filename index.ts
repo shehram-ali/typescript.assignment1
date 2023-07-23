a2390671c159e48a5abc31be52392dae0e43c395
shehram-ali <66680856+shehram-ali@users.noreply.github.com>
2023-07-23 23:29:25 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,18 @@
+
+let personname:string="shehram";
+
+console.log(personname.toUpperCase());
+console.log(personname.toLowerCase());
+console.log("Titlecase: "+ toTitleCase(personname));
+
+function toTitleCase(str: string): string {
+    return ("Shehram Ali");
+  }
+ 
+  
+  
+  
+  
+  
+  
+  
